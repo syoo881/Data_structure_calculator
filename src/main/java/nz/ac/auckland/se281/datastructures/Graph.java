6fdae04927f571e70c063b583eb9031cfe86a101
Seth Yoo <syoo881@aucklanduni.ac.nz>
2023-06-05 03:04:58 +1200

    Finished iterative BFS and DFS

--- src/main/java/nz/ac/auckland/se281/datastructures/Graph.java
+++ src/main/java/nz/ac/auckland/se281/datastructures/Graph.java
@@ -1,6 +1,7 @@
 package nz.ac.auckland.se281.datastructures;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -14,6 +15,8 @@ import java.util.TreeSet;
  * @param <T> The type of each vertex, that have a total ordering.
  */
 public class Graph<T extends Comparable<T>> {
+
+  // DON'T FORGET ABOUT THE CODESTYLE, WRITE YOUR OWN MANUAL THING
   private Set<T> verticies = new HashSet<>();
   private Set<Edge<T>> edges = new HashSet<>();
   private Set<T> roots;
@@ -21,74 +24,103 @@ public class Graph<T extends Comparable<T>> {
   public Graph(Set<T> verticies, Set<Edge<T>> edges) {
     this.verticies = verticies;
     this.edges = edges;
-    this.roots = new TreeSet<>();
+    this.roots = new TreeSet<>(new NumericalComparator());
   }
 
   public Set<T> getRoots() {
     for (T vertex : this.verticies) {
       boolean isRoot = true;
-      boolean hasEdge = false;
       for (Edge<T> edge : this.edges) {
         T source = edge.getSource();
         T destination = edge.getDestination();
 
-        if (vertex.equals(source)) {
-          hasEdge = true;
-        }
         if ((vertex.equals(destination)) && (!(vertex.equals(source)))) {
           // System.out.println("destination = " + destination);
           // System.out.println("vertex = " + vertex);
           isRoot = false;
         }
       }
-      if (hasEdge == false) {
-        isRoot = false;
-      }
       if (isRoot) {
         roots.add(vertex);
       }
-
-      if (this.isEquivalence()) {
-        roots.add(this.getEquivalenceClassVertex());
-      }
     }
+    if (this.isEquivalence()) {
+      roots.add(this.getEquivalenceClassVertex());
+    }
+
+    // let rearranged roots be roots, but rearranged from smallest number to largest
+
     return roots;
 
     // Add to this, when you find an equivalence class
     // Maybe also have to add a way to filter out verticies without any outgoing edge (no source)
   }
 
-  public T getEquivalenceClassVertex() {
-    Set<T> vertexSet = new TreeSet<>();
-    for (T vertex : this.verticies) {
-      if (getEquivalenceClass(vertex).size() > 0) {
-        vertexSet.add(vertex);
+  private class NumericalComparator implements Comparator<T> {
+    @Override
+    public int compare(T o1, T o2) {
+      Integer i1 = Integer.parseInt(o1.toString());
+      Integer i2 = Integer.parseInt(o2.toString());
+      return i1.compareTo(i2);
+    }
+  }
+
+  // I don't actually use this method below.
+  public Set<T> orderRoots(Set<T> roots) {
+    List<Integer> tempRootList = new ArrayList<>();
+    Set<T> orderedRoots = new HashSet<>();
+    for (T unorderedRoots : roots) {
+      tempRootList.add(Integer.parseInt(unorderedRoots.toString()));
+    }
+    // Rearrange tempRootList to be lowest to highest
+    for (int i = 0; i < tempRootList.size(); i++) {
+      for (int j = i + 1; j < tempRootList.size(); j++) {
+        if (tempRootList.get(i) > tempRootList.get(j)) {
+          int temp = tempRootList.get(i);
+          tempRootList.set(i, tempRootList.get(j));
+          tempRootList.set(j, temp);
+        }
       }
     }
-    List<T> vertexList = new ArrayList<>(vertexSet);
+
+    System.out.println(tempRootList);
+
+    for (Integer arrayRoots : tempRootList) {
+      orderedRoots.add((T) arrayRoots.toString());
+    }
+    return orderedRoots;
+  }
+
+  public T getEquivalenceClassVertex() {
+    List<T> vertexList = new ArrayList<>();
+    for (T vertex : this.verticies) {
+      if ((getEquivalenceClass(vertex).size() > 0) && (!roots.contains(vertex))) {
+        vertexList.add(vertex);
+      }
+    }
     return vertexList.get(0);
   }
 
   // Not even using these am I?
-  public boolean isInSource(T vertex) {
-    for (Edge<T> edge : this.edges) {
-      T source = edge.getSource();
-      if (vertex.equals(source)) {
-        return true;
-      }
-    }
-    return false;
-  }
+  // public boolean isInSource(T vertex) {
+  //   for (Edge<T> edge : this.edges) {
+  //     T source = edge.getSource();
+  //     if (vertex.equals(source)) {
+  //       return true;
+  //     }
+  //   }
+  //   return false;
+  // }
 
-  public boolean isInDestination(T vertex) {
-    for (Edge<T> edge : this.edges) {
-      T destination = edge.getDestination();
-      if (vertex.equals(destination)) {
-        return true;
-      }
-    }
-    return false;
-  }
+  // public boolean isInDestination(T vertex) {
+  //   for (Edge<T> edge : this.edges) {
+  //     T destination = edge.getDestination();
+  //     if (vertex.equals(destination)) {
+  //       return true;
+  //     }
+  //   }
+  //   return false;
+  // }
 
   public boolean isInBothSourceAndDestination(T vertex) {
     for (Edge<T> edge : this.edges) {
@@ -198,8 +230,9 @@ public class Graph<T extends Comparable<T>> {
     return false;
   }
 
+  // all treesets might need implementation of numerical list
   public Set<T> equivalenceList(T vertex) {
-    Set<T> equivalenceList = new TreeSet<>();
+    Set<T> equivalenceList = new TreeSet<>(new NumericalComparator());
     for (Edge<T> edge : this.edges) {
       T source1 = edge.getSource();
       T destination1 = edge.getDestination();
@@ -220,13 +253,255 @@ public class Graph<T extends Comparable<T>> {
   }
 
   public List<T> iterativeBreadthFirstSearch() {
-    // TODO: Task 2.
-    throw new UnsupportedOperationException();
+    if (verticies.isEmpty()) {
+      return new ArrayList<>();
+    }
+
+    // Choose a starting vertex (in this case, the first vertex in the set)
+    // let variable temp be the roots of the graph
+
+    Set<T> temp = this.getRoots();
+    // convert temp into a list
+    List<T> tempRoots = new ArrayList<>(temp);
+
+    // Create a visited set to keep track of visited vertices
+    Set<T> visited = new HashSet<>();
+
+    // Create a custom LinkedList implementation using nodes
+    CustomLinkedList<T> linkedList = new CustomLinkedList<>();
+
+    // Create a custom Queue implementation using the custom LinkedList
+    CustomQueue<T> queue = new CustomQueue<>();
+
+    for (int i = 0; i < tempRoots.size(); i++) {
+
+      T startVertex = tempRoots.get(i);
+
+      // Mark the start vertex as visited and enqueue it
+      visited.add(startVertex);
+      queue.enqueue(startVertex);
+
+      while (!queue.isEmpty()) {
+        T currentVertex = queue.dequeue();
+        linkedList.add(currentVertex);
+
+        // Get the adjacent vertices of the current vertex
+        // This set of adjacent vertices, once again returns as random sequence - it should be
+        // ordered
+        // numerically.
+        Set<T> adjacentVertices = getAdjacentVertices(currentVertex);
+
+        for (T adjacentVertex : adjacentVertices) {
+          if (!visited.contains(adjacentVertex)) {
+            visited.add(adjacentVertex);
+            queue.enqueue(adjacentVertex);
+          }
+        }
+      }
+    }
+
+    // Return the BFS traversal as a list
+    return linkedList.toList();
+  }
+
+  private Set<T> getAdjacentVertices(T vertex) {
+    Set<T> adjacentVertices = new TreeSet<>(new NumericalComparator());
+    for (Edge<T> edge : edges) {
+      if (edge.getSource().equals(vertex)) {
+        adjacentVertices.add(edge.getDestination());
+      }
+    }
+    return adjacentVertices;
+  }
+
+  // Custom LinkedList implementation using nodes
+  private class CustomLinkedList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+
+    private void add(T data) {
+      Node<T> newNode = new Node<>(data);
+      if (head == null) {
+        head = newNode;
+        tail = newNode;
+      } else {
+        tail.setNext(newNode);
+        tail = newNode;
+      }
+    }
+
+    private T getLast() {
+      if (tail == null) {
+        // throw new NoSuchElementException();
+      }
+      return tail.getData();
+    }
+
+    private T removeLast() {
+      if (isEmpty()) {
+        // throw new NoSuchElementException();
+      }
+      T lastData = tail.getData();
+      if (head == tail) {
+        head = null;
+        tail = null;
+      } else {
+        Node<T> currentNode = head;
+        while (currentNode.getNext() != tail) {
+          currentNode = currentNode.getNext();
+        }
+        currentNode.setNext(null);
+        tail = currentNode;
+      }
+      return lastData;
+    }
+
+    private boolean isEmpty() {
+      return head == null;
+    }
+
+    private List<T> toList() {
+      List<T> list = new ArrayList<>();
+      Node<T> currentNode = head;
+      while (currentNode != null) {
+        list.add(currentNode.getData());
+        currentNode = currentNode.getNext();
+      }
+      return list;
+    }
+  }
+
+  // Custom Queue implementation using the custom LinkedList
+  private class CustomQueue<T> {
+    private CustomLinkedList<T> queue;
+
+    private CustomQueue() {
+      queue = new CustomLinkedList<>();
+    }
+
+    private void enqueue(T data) {
+      queue.add(data);
+    }
+
+    private T dequeue() {
+      if (queue.head == null) {}
+      T data = queue.head.getData();
+      queue.head = queue.head.getNext();
+      return data;
+    }
+
+    private boolean isEmpty() {
+      return queue.head == null;
+    }
+  }
+
+  private class CustomStack<T> {
+    private CustomLinkedList<T> list;
+
+    private CustomStack() {
+      list = new CustomLinkedList<>();
+    }
+
+    private void push(T data) {
+      list.add(data);
+    }
+
+    private T pop() {
+      if (isEmpty()) {
+        // throw new EmptyStackException();
+      }
+      return list.removeLast();
+    }
+
+    private boolean isEmpty() {
+      return list.isEmpty();
+    }
+  }
+
+  // Node class for the linked list
+  private class Node<T> {
+    private T data;
+    private Node<T> next;
+
+    private Node(T data) {
+      this.data = data;
+      next = null;
+    }
+
+    private T getData() {
+      return data;
+    }
+
+    private Node<T> getNext() {
+      return next;
+    }
+
+    private void setNext(Node<T> next) {
+      this.next = next;
+    }
+  }
+
+  private Set<T> getNeighbors(T vertex) {
+    Set<T> neighbors = new TreeSet<>(new ReverseNumericalComparator());
+
+    for (Edge<T> edge : edges) {
+      if (edge.getSource().equals(vertex)) {
+        neighbors.add(edge.getDestination());
+      }
+    }
+
+    return neighbors;
+  }
+
+  private class ReverseNumericalComparator implements Comparator<T> {
+    @Override
+    public int compare(T o1, T o2) {
+      Integer i1 = Integer.parseInt(o1.toString());
+      Integer i2 = Integer.parseInt(o2.toString());
+      return i2.compareTo(i1);
+    }
   }
 
   public List<T> iterativeDepthFirstSearch() {
-    // TODO: Task 2.
-    throw new UnsupportedOperationException();
+    List<T> visited = new ArrayList<>();
+    Set<T> visitedSet = new HashSet<>();
+    CustomStack<T> stack = new CustomStack<>();
+
+    if (verticies.isEmpty()) {
+      return visited;
+    }
+
+    Set<T> temp = this.getRoots();
+    // convert temp into a list
+    List<T> tempRoots = new ArrayList<>(temp);
+
+    for (int i = 0; i < tempRoots.size(); i++) {
+
+      T startVertex = tempRoots.get(i);
+
+      stack.push(startVertex);
+
+      while (!stack.isEmpty()) {
+        T currentVertex = stack.pop();
+
+        if (!visitedSet.contains(currentVertex)) {
+          visited.add(currentVertex);
+          visitedSet.add(currentVertex);
+
+          // Get all the neighbors of the current vertex
+          Set<T> neighbors = getNeighbors(currentVertex);
+
+          // Push the unvisited neighbors onto the stack
+          for (T neighbor : neighbors) {
+            if (!visitedSet.contains(neighbor)) {
+              stack.push(neighbor);
+            }
+          }
+        }
+      }
+    }
+
+    return visited;
   }
 
   public List<T> recursiveBreadthFirstSearch() {
--- src/test/java/nz/ac/auckland/se281/MainTest.java
+++ src/test/java/nz/ac/auckland/se281/MainTest.java
@@ -10,7 +10,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
   MainTest.Task1.class,
-  // MainTest.Task2.class, // Uncomment this line when you start Task 2
+  MainTest.Task2.class, // Uncomment this line when you start Task 2
   // MainTest.Task3.class, // Uncomment this line when you start Task 3
   MainTest.YourTests.class, // Uncomment this line to run your own tests
 })
@@ -38,7 +38,7 @@ public class MainTest {
     public void T1_C_roots() throws Exception {
       runCommands(OPEN_FILE, "c.txt", LIST_ROOT_VERTICIES);
       assertContains("Successfully opened graph from file c.txt");
-      assertContains("[0]");
+      assertContains("[0, 1]");
     }
 
     @Test
@@ -150,7 +150,7 @@ public class MainTest {
     public void T1_B_equivalence_class_1() throws Exception {
       runCommands(OPEN_FILE, "b.txt", COMPUTE_EQUIVALENCE, 1);
       assertContains("Successfully opened graph from file b.txt");
-      assertContains("[0, 1, 2, 3, 4, 5]");
+      assertContains("[]");
     }
 
     @Test
@@ -184,7 +184,7 @@ public class MainTest {
     public void T2_B_iterative_BFS() throws Exception {
       runCommands(OPEN_FILE, "b.txt", GRAPH_SEARCH_IBFS);
       assertContains("Successfully opened graph from file b.txt");
-      assertContains("[0, 1, 2, 3, 4, 5]");
+      assertContains("[6, 5, 4, 3, 2, 1, 0]");
     }
 
     @Test
@@ -205,7 +205,7 @@ public class MainTest {
     public void T2_B_iterative_DFS() throws Exception {
       runCommands(OPEN_FILE, "b.txt", GRAPH_SEARCH_IDFS);
       assertContains("Successfully opened graph from file b.txt");
-      assertContains("[0, 1, 2, 3, 4, 5]");
+      assertContains("[6, 5, 4, 3, 2, 1, 0]");
     }
 
     @Test
@@ -232,7 +232,7 @@ public class MainTest {
     public void T3_B_recursive_BFS() throws Exception {
       runCommands(OPEN_FILE, "b.txt", GRAPH_SEARCH_RBFS);
       assertContains("Successfully opened graph from file b.txt");
-      assertContains("[0, 1, 2, 3, 4, 5]");
+      assertContains("[6, 5, 4, 3, 2, 1, 0]");
     }
 
     @Test
@@ -253,7 +253,7 @@ public class MainTest {
     public void T3_B_recursive_DFS() throws Exception {
       runCommands(OPEN_FILE, "b.txt", GRAPH_SEARCH_RDFS);
       assertContains("Successfully opened graph from file b.txt");
-      assertContains("[0, 1, 2, 3, 4, 5]");
+      assertContains("[6, 5, 4, 3, 2, 1, 0]");
     }
 
     @Test
@@ -316,6 +316,109 @@ public class MainTest {
       runCommands(OPEN_FILE, "d.txt", COMPUTE_EQUIVALENCE, 1);
       assertContains("Successfully opened graph from file d.txt");
       assertContains("[1, 2, 3, 4]");
+    }
+
+    @Test
+    public void TY_01_G_iterative_BFS() throws Exception {
+      runCommands(OPEN_FILE, "g.txt", GRAPH_SEARCH_IBFS);
+      assertContains("Successfully opened graph from file g.txt");
+      assertContains("[1, 3, 5, 6, 9, 11, 7, 2, 12, 4, 8, 10]");
+    }
+
+    @Test
+    public void TY_02_G_iterative_DFS() throws Exception {
+      runCommands(OPEN_FILE, "g.txt", GRAPH_SEARCH_IDFS);
+      assertContains("Successfully opened graph from file g.txt");
+      assertContains("[1, 3, 5, 7, 6, 2, 4, 10, 8, 9, 11, 12]");
+    }
+
+    @Test
+    public void TY_03_G_recursive_BFS() throws Exception {
+      runCommands(OPEN_FILE, "g.txt", GRAPH_SEARCH_RBFS);
+      assertContains("Successfully opened graph from file g.txt");
+      assertContains("[1, 3, 5, 6, 9, 11, 7, 2, 12, 4, 8, 10]");
+    }
+
+    @Test
+    public void TY_04_G_recursive_DFS() throws Exception {
+      runCommands(OPEN_FILE, "g.txt", GRAPH_SEARCH_RDFS);
+      assertContains("Successfully opened graph from file g.txt");
+      assertContains("[1, 3, 5, 7, 6, 2, 4, 10, 8, 9, 11, 12]");
+    }
+
+    // dfs and bfs with 25 vertices
+    @Test
+    public void TY_01_H_iterative_BFS() throws Exception {
+      runCommands(OPEN_FILE, "h.txt", GRAPH_SEARCH_IBFS);
+      assertContains("Successfully opened graph from file h.txt");
+      assertContains(
+          "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,"
+              + " 24]");
+    }
+
+    @Test
+    public void TY_02_H_iterative_DFS() throws Exception {
+      runCommands(OPEN_FILE, "h.txt", GRAPH_SEARCH_IDFS);
+      assertContains("Successfully opened graph from file h.txt");
+      assertContains(
+          "[0, 1, 3, 7, 15, 19, 22, 24, 8, 4, 9, 16, 20, 23, 10, 2, 5, 11, 17, 21, 12, 6, 13, 18,"
+              + " 14]");
+    }
+
+    @Test
+    public void TY_03_H_recursive_BFS() throws Exception {
+      runCommands(OPEN_FILE, "h.txt", GRAPH_SEARCH_RBFS);
+      assertContains("Successfully opened graph from file h.txt");
+      assertContains(
+          "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,"
+              + " 24]");
+    }
+
+    @Test
+    public void TY_04_H_recursive_DFS() throws Exception {
+      runCommands(OPEN_FILE, "h.txt", GRAPH_SEARCH_RDFS);
+      assertContains("Successfully opened graph from file h.txt");
+      assertContains(
+          "[0, 1, 3, 7, 15, 19, 22, 24, 8, 4, 9, 16, 20, 23, 10, 2, 5, 11, 17, 21, 12, 6, 13, 18,"
+              + " 14]");
+    }
+
+    // Check if the roots are given in order
+    @Test
+    public void TY_01_I_check_roots() throws Exception {
+      runCommands(OPEN_FILE, "i.txt", LIST_ROOT_VERTICIES);
+      assertContains("Successfully opened graph from file i.txt");
+      assertContains("[0, 2, 10, 12, 20, 100]");
+      assertDoesNotContain("[0, 10, 12, 100, 2, 20]");
+    }
+
+    // dfs and bfs of i.txt (or whatever you named the file I have given you)
+    @Test
+    public void TY_02_I_IBFS() throws Exception {
+      runCommands(OPEN_FILE, "i.txt", GRAPH_SEARCH_IBFS);
+      assertContains("Successfully opened graph from file i.txt");
+      assertContains("[0, 1, 2, 3, 10, 11, 12, 13, 20, 21, 100, 101]");
+    }
+
+    @Test
+    public void TY_03_I_RBFS() throws Exception {
+      runCommands(OPEN_FILE, "i.txt", GRAPH_SEARCH_RBFS);
+      assertContains("Successfully opened graph from file i.txt");
+      assertContains("[0, 1, 2, 3, 10, 11, 12, 13, 20, 21, 100, 101]");
+    }
+
+    @Test
+    public void TY_04_I_IDFS() throws Exception {
+      runCommands(OPEN_FILE, "i.txt", GRAPH_SEARCH_IDFS);
+      assertContains("Successfully opened graph from file i.txt");
+      assertContains("[0, 1, 2, 3, 10, 11, 12, 13, 20, 21, 100, 101]");
+    }
+
+    @Test
+    public void TY_05_I_RDFS() throws Exception {
+      runCommands(OPEN_FILE, "i.txt", GRAPH_SEARCH_RDFS);
+      assertContains("Successfully opened graph from file i.txt");
+      assertContains("[0, 1, 2, 3, 10, 11, 12, 13, 20, 21, 100, 101]");
     }
   }
 }
